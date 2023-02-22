f15a5e7ab73e05ea0d16fef986adf3ca250e3e23
Lucas <lucas.7795.la@gmail.com>
2023-02-22 19:52:33 -0300

    Crianção das classes Funcionario e TestaFuncionario

---
+++ src/TestaFuncionario.java
@@ -0,0 +1,11 @@
+public class TestaFuncionario {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Lucas de Almeida Pereira");
+        funcionario.setCpf("123.456.789-56");
+        funcionario.setSalario(2840.25);
+
+        System.out.println("O nome do funcionário é " + funcionario.getNome());
+        System.out.println("Sua bonificação é de R$" + funcionario.getBonificacao());
+    }
+}
